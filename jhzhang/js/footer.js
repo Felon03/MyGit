17ab0acb13470a9aeb4d3de75fb360a7d0827861
FreedomLy <freedom.jfl@gmail.com>
2018-10-26 10:29:19 +0800

    remove files

--- jhzhang/js/footer.js
+++
@@ -1,5 +0,0 @@
-var year = new Date()
-document.write("        <div class=\'footer\'>");
-document.write("            <p class=\'copyright\'>Copyright &copy 2015 - " + year.getFullYear() + " Jianhui Zhang. All rights reserved.</p>");
-document.write("            <p>Create by Zhi Li, JFL.</p>");
-document.write("        </div>");
